c6afe857675e05fc7a63273f552c33cb578959a0
Michael Atkisson <mpatkisson@gmail.com>
2019-06-10 13:05:05 -0700

    Adds an Attributions page

--- src/Web/Controllers/HomeController.cs
+++ src/Web/Controllers/HomeController.cs
@@ -15,6 +15,11 @@ namespace Web.Controllers
             return View();
         }
 
+        public IActionResult Attributions()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
