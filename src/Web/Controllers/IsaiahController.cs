63facd03f32116d2d3997fb9b864887880f53905
Michael Atkisson <mpatkisson@gmail.com>
2019-06-12 00:36:06 -0700

    Adds the book of Isaiah to the API

---
+++ src/Web/Controllers/IsaiahController.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [ApiController]
+    public class IsaiahController : BookController
+    {
+        
+        public override string BookName => "Isa";
+        
+    }
+}
