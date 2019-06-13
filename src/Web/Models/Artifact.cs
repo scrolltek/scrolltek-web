3aeee1701cca4504aacaf7ecf78ba2af61514151
Michael Atkisson <mpatkisson@gmail.com>
2019-06-12 17:58:02 -0700

    Converts traditional numbers to Hebrew Gematria

--- src/Web/Models/Artifact.cs
+++ src/Web/Models/Artifact.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Scrolltek.Web.Models
@@ -15,9 +16,38 @@ namespace Scrolltek.Web.Models
         /// </summary>
         public int OrderNumber { get; set; }
 
+        public string HebrewOrdinal
+        {
+            get
+            {
+                return EllisWeb.Gematria.Calculator.ConvertToGematriaNumericString(OrderNumber, false);
+            }
+        }
+
         /// <summary>
         /// Gets or sets the actual text of the artifact.
         /// </summary>
         public abstract string Text { get; }
+
+        public static string FormatHebrew(int num)
+        {
+            if (num <= 0)
+                throw new ArgumentOutOfRangeException();
+            StringBuilder ret = new StringBuilder(new string('ת', num / 400));
+            num %= 400;
+            if (num >= 100)
+            {
+                ret.Append("קרש"[num / 100 - 1]);
+                num %= 100;
+            }
+            if (num >= 10)
+            {
+                ret.Append("כלמנסעפצי"[num / 10 - 1]);
+                num %= 10;
+            }
+            if (num > 0)
+                ret.Append("אבגדהוזחט"[num - 1]);
+            return ret.ToString();
+        }
     }
 }
